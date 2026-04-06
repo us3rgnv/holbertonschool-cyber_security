09abb54ab41db6c5e060b6c1dddb6eb493570056
us3rgnv <you@example.com>
2026-04-06 08:52:14 -0400

    web

---
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <log_file>"
+    exit 1
+fi
+
+logfile=$1
+
+attacker=$(awk '{print $1}' "$logfile" | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')
+
+awk -v ip="$attacker" '$1 == ip {count++} END {print count}' "$logfile"
